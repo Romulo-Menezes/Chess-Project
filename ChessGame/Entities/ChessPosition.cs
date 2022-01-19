8d862923d667160a63334bce606cc20ac4c3636d
romul0 <romulo.m.menezes@hotmail.com>
2022-01-19 15:56:40 -0300

    Added ChessPosition class

---
+++ ChessGame/Entities/ChessPosition.cs
@@ -0,0 +1,20 @@
+﻿namespace ChessGame.Entities
+{
+    class ChessPosition
+    {
+        public int Row { get; set; }
+        public char Column { get; set; }
+
+        public ChessPosition(int row, char column)
+        {
+            Row = row;
+            Column = column;
+        }
+
+        public Position ToPosition()
+        {
+            return new Position(8 - Row, Column - 'a');
+        }
+
+    }
+}
