97d651a7bff39e88256545ed1a3f37fa9b727c67
romul0 <romulo.m.menezes@hotmail.com>
2022-01-25 17:46:07 -0300

    Special move Castling implemented

--- ChessGame/Entities/Pieces/King.cs
+++ ChessGame/Entities/Pieces/King.cs
@@ -1,16 +1,26 @@
 ﻿using ChessGame.Entities.Enums;
+using ChessGame.Controller;
 
 namespace ChessGame.Entities
 {
     class King : Piece
     {
-        public King(Board board, Color color) : base(board, color)
+        private ChessGameController _chessGame;
+
+        public King(Board board, Color color, ChessGameController chessGame) : base(board, color)
         {
+            _chessGame = chessGame;
         }
 
         public override string ToString()
         {
             return "K";
+        }
+
+        private bool CastlingTest(Position position)
+        {
+            Piece piece = Board.GetPiece(position);
+            return piece != null && piece is Rook && piece.Color == Color && piece.QtyMoves == 0;
         }
 
         public override bool[,] AvailableMovements()
@@ -66,6 +76,35 @@ namespace ChessGame.Entities
             {
                 movements[pos.Row, pos.Column] = true;
             }
+
+            // Special move: Castling
+
+            if(QtyMoves == 0 && !_chessGame.Check)
+            {
+                Position rookPos_1 = new Position(GetRow(), GetColumn() + 3);
+                if (CastlingTest(rookPos_1))
+                {
+                    Position pos1 = new Position(GetRow(), GetColumn() + 1);
+                    Position pos2 = new Position(GetRow(), GetColumn() + 2);
+                    if(Board.GetPiece(pos1) == null && Board.GetPiece(pos2) == null)
+                    {
+                        movements[pos2.Row, pos2.Column] = true;
+                    }
+                }
+
+                Position rookPos_2 = new Position(GetRow(), GetColumn() - 4);
+                if (CastlingTest(rookPos_2))
+                {
+                    Position pos1 = new Position(GetRow(), GetColumn() - 1);
+                    Position pos2 = new Position(GetRow(), GetColumn() - 2);
+                    Position pos3 = new Position(GetRow(), GetColumn() - 3);
+                    if (Board.GetPiece(pos1) == null && Board.GetPiece(pos2) == null && Board.GetPiece(pos3) == null)
+                    {
+                        movements[pos2.Row, pos2.Column] = true;
+                    }
+                }
+            }
+
 
             return movements;
         }
